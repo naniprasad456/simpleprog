0c769110a34b489e173e7bc4dde4ecba771f3ea0
naniprasad456 <ganta@DESKTOP-PE2Q8QL>
2022-05-19 05:18:47 +0530

    small simple program

---
+++ simpleprog/src/simpleprogram.java
@@ -0,0 +1,9 @@
+
+public class simpleprogram {
+	public static void main(String[] args) {  
+	    //Code of Java for loop  
+	    for(int i=1;i<=10;i++){  
+	        System.out.println(i);  
+	    }  
+	}  
+}
