292dad7f77c2e147f196333e191a580f8fb9a7f3
naniprasad456 <ganta@DESKTOP-PE2Q8QL>
2022-05-19 05:30:34 +0530

    added simpletest.java

--- simpleprog/src/simpleprog/simpletest.java
+++ simpleprog/src/simpleprog/simpletest.java
@@ -1,10 +1,12 @@
+package simpleprog;
 
-public class simpleprogram {
+public class simpletest {
 		public static void main(String[] args) {  
 		    //Code of Java for loop  
 		    for(int i=1;i<=10;i++){  
 		        System.out.println(i);  
-		   }  
-	}  
+		    }  
+		}  
 }
+
 
